4d9da78b4aeb943df80df0eed3affe6909ffbd05
Jim Crawford <jim@mycodinglife.com>
2025-06-06 10:15:54 -0500

    Commit of core tooling and build scripts for a minimal image.

--- Containerfile
+++ Containerfile
@@ -3,25 +3,15 @@ FROM scratch AS ctx
 COPY build_files /
 
 # Base Image
-FROM ghcr.io/ublue-os/bazzite:stable
+FROM quay.io/fedora/fedora-kinoite:latest
 
-## Other possible base images include:
-# FROM ghcr.io/ublue-os/bazzite:latest
-# FROM ghcr.io/ublue-os/bluefin-nvidia:stable
-# 
-# ... and so on, here are more base images
-# Universal Blue Images: https://github.com/orgs/ublue-os/packages
-# Fedora base image: quay.io/fedora/fedora-bootc:41
-# CentOS base images: quay.io/centos-bootc/centos-bootc:stream10
-
-### MODIFICATIONS
-## make modifications desired in your image and install packages by modifying the build.sh script
-## the following RUN directive does all the things required to run "build.sh" as recommended.
 
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
+    cp -r /ctx/etc/* /etc && \
+    cp -r /ctx/usr/* /usr && \
     /ctx/build.sh && \
     ostree container commit
     
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,19 @@
 
 set -ouex pipefail
 
+### Remove Packages
+dnf remove -y \
+    firefox \
+    firefox-langpacks \
+    plasma-discover-rpm-ostree \
+    toolbox
+
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf install distrobox ksshaskpass libvirt-daemon-config-network libvirt-daemon-kvm swtpm-selinux qemu-kvm virt-manager -y
+dnf install fish borgbackup solaar plasma-firewall-firewalld -y
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+## Enable automatic update staging.
+sed -i 's/#AutomaticUpdatePolicy.*/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
+systemctl enable rpm-ostreed-automatic.timer
+systemctl enable swtpm-workaround
