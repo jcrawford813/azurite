aa03578fa869057a0553a2ec92ad59b9e98df3a1
Jim Crawford <jim@mycodinglife.com>
2025-07-20 12:18:18 -0500

    Add multicast dns config. Bump Firefox PWA version. Add codecs to use native firefox over flatpak.

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,15 +4,24 @@ set -ouex pipefail
 
 ### Remove Packages
 dnf remove -y \
-    firefox \
-    firefox-langpacks \
     plasma-discover-rpm-ostree \
     toolbox
 
 dnf update -y
 
+#Install RPM Fusion & Codecs
+dnf install -y \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+dnf install -y \
+    libheif-freeworld
+
+dnf swap ffmpeg-free ffmpeg --allowerasing -y
+dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y
+dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
+
 #Install Firefox PWA Runtime
-curl -fsSL https://github.com/filips123/PWAsForFirefox/releases/download/v2.14.1/firefoxpwa-2.14.1-1.x86_64.rpm -o /tmp/firefoxpwa.rpm
+curl -fsSL https://github.com/filips123/PWAsForFirefox/releases/download/v2.15.0/firefoxpwa-2.15.0-1.x86_64.rpm -o /tmp/firefoxpwa.rpm
 dnf --setopt=install_weak_deps=False install -y /tmp/firefoxpwa.rpm
 
 ### Install packages
