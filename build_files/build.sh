1173a5e6025fb928f515301ddb5578f945fe408c
Jim Crawford <jim@mycodinglife.com>
2026-04-03 17:00:45 -0500

    Add VPN and virtualization libraries.

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,6 +13,9 @@ dnf remove -y \
 
 dnf update -y
 
+### Add VPN Repo
+dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
+
 ## Add RPM Fusion Repositories and working libheif
 dnf install -y \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
@@ -21,6 +24,7 @@ dnf install -y libheif-freeworld
 
 ### Install packages (Distrobox, Fish, Backup Solution)
 dnf install distrobox ksshaskpass fish borgbackup solaar fluidsynth lm_sensors -y
+dnf install -y mullvad-vpn libvirt qemu-kvm swtpm
 
 ## Enable workaround for qemu/kwm swtpm issue
 systemctl enable swtpm-workaround
